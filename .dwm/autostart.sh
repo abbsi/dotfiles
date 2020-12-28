d3975c75dc8359440550c5b7bfe43adc1f133636
abbsi <nabil.abbas@gmail.com>
2020-12-28 09:03:21 +0400

    [system] added xidlehook to auto lock system

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,8 +1,29 @@
-#! /bin/bash 
+#!/usr/bin/env bash
+
 picom &
 nitrogen --restore &
+numlockx &
+
+# https://github.com/jD91mZM2/xidlehook
+xidlehook \
+  --not-when-fullscreen \
+  --not-when-audio \
+  --timer 300 \
+    "xrandr --output DP-2 --brightness .8 --output HDMI-0 --brightness .8 --output DP-0 --brightness .8" \
+    "xrandr --output DP-2 --brightness  1 --output HDMI-0 --brightness  1 --output DP-0 --brightness  1" \
+  --timer 60 \
+    "xrandr --output DP-2 --brightness .6 --output HDMI-0 --brightness .6 --output DP-0 --brightness .6" \
+    "xrandr --output DP-2 --brightness  1 --output HDMI-0 --brightness  1 --output DP-0 --brightness  1" \
+  --timer 30 \
+    "xrandr --output DP-2 --brightness .4 --output HDMI-0 --brightness .4 --output DP-0 --brightness .4" \
+    "xrandr --output DP-2 --brightness  1 --output HDMI-0 --brightness  1 --output DP-0 --brightness  1" \
+  --timer 15 \
+    "slock" \
+    "xrandr --output DP-2 --brightness  1 --output HDMI-0 --brightness  1 --output DP-0 --brightness  1" &
+    
+dunstify -u low "Completed DWM Autostart.sh" &
+
+## Alterntive Options for status bars or auto locking
 # slstatus &
 # dwmblocks &
-numlockx &
-dunstify -u low "Completed DWM Autostart.shxx" &
-
+# xautolock -time 5 -locker "slock" -detectsleep &
