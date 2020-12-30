4eb145ccf56b452d7ee8ce7c0447e74008734abf
abbsi <nabil.abbas@gmail.com>
2020-12-30 18:45:41 +0400

    [polybar] get HWMON and INET interface paths

--- .config/polybar/scripts/launch-polybar.sh
+++ .config/polybar/scripts/launch-polybar.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-rm -rf /tmp/polybar_mqueue.* && echo ">> Removed mqueue" | tee -a /tmp/polybar-all.log
-killall -q polybar && echo ">> Killed All Bars" | tee -a /tmp/polybar-all.log
+# Export network interface with Internet
+export INET_IFACE=$(ip addr show | awk '/inet.*brd/{print $NF; exit}')
+echo ">> Interface Name: $INET_IFACE" | tee -a /tmp/polybar-all.log
 
-bars=(main1 main2 main3 left right)
-for bar in ${bars[@]}; do
-  
-  echo "==> Launching $bar" | tee -a /tmp/polybar-$bar.log
-  $(polybar --reload $bar | tee -a /tmp/polybar-$bar.log) & disown
+# Export path to AMD Tctl for temperature module
+for path in /sys/class/hwmon/hwmon*/temp*_label; do
+    label="$(cat "$path")"
+
+    if [[ "$label" == "Tctl" ]]; then
+        export HWMON_PATH="${path%_label}_input"
+        echo ">> HWMON Path: $HWMON_PATH" | tee -a /tmp/polybar-all.log
+    fi
 done
 
-# polybar --reload main2 >>/tmp/polybar-all.log 2>&1 &
-# polybar --reload main3 >>/tmp/polybar-all.log 2>&1 &
-# polybar --reload left  >>/tmp/polybar-all.log 2>&1 &
-# polybar --reload right >>/tmp/polybar-all.log 2>&1 &
+# Remove old IPC
+rm -rf /tmp/polybar_mqueue.* && echo ">> Removed mqueue" | tee -a /tmp/polybar-all.log
+
+# Kill bars
+killall -q polybar && echo ">> Killed All Bars" | tee -a /tmp/polybar-all.log
+
+# Load array of bars
+bars=(main1 main2 main3 left right)
+for bar in ${bars[@]}; do
+  echo "==> Launching $bar" | tee -a /tmp/polybar-$bar.log
+  $(polybar --reload $bar | tee -a /tmp/polybar-$bar.log) &
+done
