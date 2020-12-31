24dbd4c79bf8cd9beaf3bac0675c6a1fed2c3fee
abbsi <nabil.abbas@gmail.com>
2021-01-01 02:20:15 +0400

    [misc] updated xrandr

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 3840x160 --rotate normal --output HDMI-0 --primary --mode 3840x1600 --pos 0x0 --rotate normal 
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 0x80 --rotate normal --output DP-2 --primary --mode 3840x1600 --pos 2565x0 --rotate normal --output DP-0 --mode 2560x1440 --pos 6410x80 --rotate normal
