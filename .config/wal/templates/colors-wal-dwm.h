c3d9bdd51a3d4ded705036d0ea4cf22833792337
abbsi <nabil.abbas@gmail.com>
2021-01-05 17:03:40 +0400

    [pywal] added dwm config color template

---
+++ .config/wal/templates/colors-wal-dwm.h
@@ -0,0 +1,28 @@
+static const char wal_norm_fg[] = "{color15}";
+static const char wal_norm_bg[] = "{color0}";
+static const char wal_norm_border[] = "{color8}";
+
+static const char wal_sel_fg[] = "{color15}";
+static const char wal_sel_bg[] = "{color2}";
+static const char wal_sel_border[] = "{color1}";
+
+static const char wal_urg_fg[] = "{color15}";
+static const char wal_urg_bg[] = "{color1}";
+static const char wal_urg_border[] = "{color1}";
+
+static const char color00[] = "{color0}";
+static const char color01[] = "{color1}";
+static const char color02[] = "{color2}";
+static const char color03[] = "{color3}";
+static const char color04[] = "{color4}";
+static const char color05[] = "{color5}";
+static const char color06[] = "{color6}";
+static const char color07[] = "{color7}";
+static const char color08[] = "{color8}";
+static const char color09[] = "{color9}";
+static const char color10[] = "{color10}";
+static const char color11[] = "{color11}";
+static const char color12[] = "{color12}";
+static const char color13[] = "{color13}";
+static const char color14[] = "{color14}";
+static const char color15[] = "{color15}";
