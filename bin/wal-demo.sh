f68a82429faa5a0f6c443413beede49d633fa361
abbsi <nabil.abbas@gmail.com>
2021-01-07 14:34:15 +0400

    [scripts] modified wal-demo to take screenshots

--- bin/wal-demo.sh
+++ bin/wal-demo.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+a=1
 for f in ~/.wallpapers/*; do 
   echo "Processing $f file.."
   wal -i $f --backend schemer2
   sleep 2
   rofi-app &
   sleep 2
+  padded=$(printf "%04d" "$a")
+  $(scrot -a 0,80,2560,1440 /home/abbsi/.wallpapers/screens/$padded-%Y%m%d-%s.png)
+  sleep 2
   xdotool key Escape
+  let a=a+1
 done
